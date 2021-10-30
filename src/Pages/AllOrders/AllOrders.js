d2257edc41c9526d0f2871a604252af80a90745b
ahmedparbes <fbhelpsbd@gmail.com>
2021-10-30 15:19:52 +0600

    add

--- src/Pages/AllOrders/AllOrders.js
+++ src/Pages/AllOrders/AllOrders.js
@@ -13,6 +13,31 @@ const AllOrders = () => {
             .then(data => setOrders(data))
     }, []);
 
+    const cancelOrder = id => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `https://aqueous-tundra-43046.herokuapp.com/users/${id}`
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount) {
+
+                        alert('Order cancel successfully')
+                        const remain = orders.filter(order => order._id !== id)
+                        setOrders(remain);
+                    }
+
+                })
+        }
+
+
+    }
+
+
+
     return (
         <Container>
             <h2 className="manage-order-font text-primary font">This is the orders, what we collected</h2>
@@ -39,7 +64,7 @@ const AllOrders = () => {
                                     <td>{order.email}</td>
                                     <td>{order._id}</td>
                                     <td>{order.productName}</td>
-                                    {/* <td><button onClick={() => cancelOrder(user._id)}><i className="fas fa-trash-alt"> cancel order</i></button></td> */}
+                                    <td><button onClick={() => cancelOrder(order._id)}><i className="fas fa-trash-alt"> cancel order</i></button></td>
                                 </tr>
 
                             </table>
@@ -50,4 +75,4 @@ const AllOrders = () => {
     );
 };
 
-export default AllOrders
+export default AllOrders;
